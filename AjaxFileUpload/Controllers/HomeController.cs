5a22009cf2fc203645073f2fa8c71465468257e2
Ishak Alpfidan <isaac.alpines@hotmail.com>
2021-08-31 13:28:15 +0300

    Project created and file upload codes written

---
+++ AjaxFileUpload/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxFileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                if (Directory.Exists(Server.MapPath("~/Files")) == false)
+                    Directory.CreateDirectory(Server.MapPath("~/Files"));
+
+                file.SaveAs(Path.Combine(Server.MapPath("~/Files"), file.FileName));
+
+                return Json(new { hasError = false, message = "File upload success..!"  });
+            }
+
+            return Json(new { hasError = true });
+        }
+    }
+}
